d639835dbe348083772bc238a56da6da709616ef
pavan kalyan karimi <pavankalyan4167@gmail.com>
2023-06-23 16:13:23 +0530

    getting data from REDUX

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from "axios";
-
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 const HomeScreen = () => {
-    const [ products, setProducts ] = useState([])
+    
+    const {data: products, isLoading, error} = useGetProductsQuery();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get("/api/products");
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
   return (
     <>
-      <h1>Latest Products</h1>
+        {isLoading ? (<h2><Loader /></h2>) : error ? (<Message variant="danger">
+        { error?.data?.message || error.error}
+        </Message>) : (<>
+            <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -24,6 +21,8 @@ const HomeScreen = () => {
             </Col>
         ))}
       </Row>
+        </>)}
+      
     </>
   )
 }
